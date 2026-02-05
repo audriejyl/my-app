33acea78f94a03f59fd7c1ee246ad05d07fc9753
audrie <20193616@s.ubaguio.edu>
2026-02-05 14:08:34 +0800

    Update app with navigation, login, and about standalone components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './comps/login/login.component';
+import { AboutComponent } from './comps/about/about.component';
+import { HomeComponent } from './comps/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
